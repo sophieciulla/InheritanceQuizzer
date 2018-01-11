596534dfb5baa0d9b2b04c432509d663ff52e69e
Sophie Ciulla <Sophie.Ciulla@MU-C1-09.mullen.pvt.k12.co.us>
2018-01-11 15:00:50 -0700

    added answer options

---
+++ Inheritance Quizzer/src/Data.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Data
+	{
+		public static void fillArray()
+		{
+			public static ArrayList<Plant>plantArray = new ArrayList<Plant>();
+			plantArray.add(new Plant());
+		}
+	
+
+	}
--- Inheritance Quizzer/src/Quizzer.java
+++ Inheritance Quizzer/src/Quizzer.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Quizzer
 	{
+		static int questions;
+		static int numberRight;
 
 		public static void main(String[] args)
 			{
@@ -9,7 +11,30 @@ public class Quizzer
 			}
 		public static void askNumberOfQuestions()
 		{
+			Scanner userInput = new Scanner(System.in);
 			System.out.println("How many questions would you like to have?");
+			questions = userInput.nextInt();
+			numberRight = 0;
+		}
+		public static void askQuestions()
+		{
+			for (int i =0;i<questions; i++)
+				{
+					System.out.println("If the code runs:
+						System.out.println("Plant x = new Plant( ); /n x.grow()");
+						
+						
+				}
+		public static void showAnswerOptions()
+		{
+					System.out.println("What will the code output?");
+					System.out.println("    A. There are some types of flowers that can grow in complete shade");
+					System.out.println("    B. I need sun to grow");
+					System.out.println("    C. If you don't water me, I will die");
+					System.out.println("    D. Flowers are usually very colorful");
+					System.out.println("    E. The code will not execute");
+				
+		}
 		}
 
 	}
